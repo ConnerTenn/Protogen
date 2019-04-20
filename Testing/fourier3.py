d28a8a8b862517ba7509cd4012722bbf0b15a423
Conner <connertenn@gmail.com>
2019-04-20 01:58:37 -0400

    Added testing version 3
    Interesting patterns are created from a glitch in this build

---
+++ Testing/fourier3.py
@@ -0,0 +1,105 @@
+
+import math
+import cmath
+import random
+
+def ceildiv(a,b):
+	return int(a/b) + (1 if a%b else 0)
+def minmax(val,low,high):
+	return min(max(val,low),high)
+
+def graph(arr,height=10,scalex=1,scaley=1):
+	n=len(arr)
+	print("-"*(int(n/scalex)+2))
+	for h in range(height,-height-1,-scaley):
+		print("|", end="")
+		for i in range(0,n,scalex):
+			v=round(arr[i])
+			if (h==10 and v>10): print("!", end="")
+			elif (h==-10 and v<-10): print("!", end="")
+			elif (v==h): print("#", end="")
+			else: print("-" if h==0 else " ", end="")
+		print("|")
+	print("-"*(int(n/scalex)+2))
+
+def graphcpx(arr,height=10,scalex=1,scaley=1):
+	n=len(arr)
+	print("-"*(int(n/scalex)+2))
+	for h in range(height,-height-1,-scaley):
+		print("|", end="")
+		for i in range(0,n,scalex):
+			v=[round(abs(arr[i])), round(cmath.phase(arr[i])/math.tau*height)]
+			if v[0] < 10**-10: v[1]=0
+			c=0
+			if (v[0]==h): c+=1
+			if (v[1]==h): c+=2
+			c=(0, '#', '\'', '@')[c]
+			if (h==10 and v[0]>10): print("!", end="")
+			elif (h==-10 and v[0]<-10): print("!", end="")
+			elif (c): print(c, end="")
+			else: print("-" if h==0 else " ", end="")
+		print("|")
+	print("-"*(int(n/scalex)+2))
+
+def fourier(arr):
+	bands=[]
+	n=len(arr)
+	m=math.ceil((n+1)/2)
+	for f in range(0,m):
+		bands.append(0)
+		for i in range(0,n):
+			bands[f]+=arr[i]*cmath.exp((0.25-f*i/n)*math.tau*1j)
+		#magnatude * normalization * last_element_adjustment
+		bands[f]=bands[f] * (2/n) * ((1,0.5)[f==0],0.5)[f==m-1]
+	return bands
+
+arr=[]
+n=100
+for i in range(0,n):
+	#v=1*math.sin(0.5*i*math.tau/n+0.2*math.tau)
+	v=2.5*math.sin(10*i*math.tau/n+0.5*math.tau) + 3*math.sin(3*i*math.tau/n+0.6*math.tau) + 2*math.sin(5*i*math.tau/n+0.9*math.tau) + (i/n-0.5)*1
+	#v=4*((int((5*i/n*2)%2))-0.5)*2
+	#v*=2*(n-i)/n
+	#v=3*math.sin(13*i*math.tau/n) + 3*math.sin(23*i*math.tau/n)
+	#v=2*math.cos(n/2*i*math.tau/n)
+	arr.append(v)
+
+graph(arr,scalex=1)
+print()
+
+bands=fourier(arr)
+graphcpx(bands)
+print()
+
+m=len(bands)
+arr2=[0]*n
+for f in range(0,m):
+	for i in range(0,n):
+		arr2[i]+=abs(bands[f])*math.sin(f*((i/n)*math.tau) + cmath.phase(bands[f])) #
+		#arr2[i]=5*math.cos(f*((i/(n/d))*math.tau) + s*f*math.tau + f/(m/2)*math.tau)
+
+graph(arr2,scalex=1)
+print()
+
+
+d=50
+m=ceildiv(n,2)
+bands=[[0]*m for i in range(0,d)]
+for i in range(0,n):
+	#l=minmax(i-d,0,d)
+	for f in range(0,min(m,i)):
+		imag = lambda i: arr[i]*cmath.exp((0.25-f*i)*math.tau*1j)
+		val = imag(i)
+		val=val+bands[1][f]#-( imag(i-d) if i-d>=0 else 0)
+
+		bands[0][f]=val# * (2/n) * (1,0.5)[f==m-1] * (1,0.5)[f==0]
+	
+	if i==0: print(abs(bands[0][0]), cmath.phase(bands[0][0])); print(abs(bands[0][1]), cmath.phase(bands[0][1]))
+
+	for f in range(0,m):
+		print("\033[48;5;" + str(int(23*abs(bands[0][f])/10+232)) + "m",end=" ")
+	print("\033[0m")
+
+	bands=[[0]*m]+bands[:-1]
+
+
