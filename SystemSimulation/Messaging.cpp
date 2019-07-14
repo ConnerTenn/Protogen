3c304599987202daa7866a01df943f899c9e110e
Conner Tenn <connertenn@gmail.com>
2019-07-14 18:57:40 -0400

    Revised Definitions

--- SystemSimulation/Messaging.cpp
+++ SystemSimulation/Messaging.cpp
@@ -4,10 +4,15 @@
 void InitMessager(MessageHandler *handler, Serial *interface)
 {
 	handler->Interface = interface;
-	handler->Head = 0;
-	handler->Tail = 0;
+	handler->MsgHead = 0; handler->MsgTail = 0;
+	handler->BuffHead = 0; handler->BuffTail = 0;
 }
-bool GetNextMessage(Message)
+bool GetNextMessage(MessageHandler *handler, Message *msg)
 {
-	
+	char buff[MSG_TOTAL_LEN*2];
+	if (Serial::Available(handler->Interface))
+	{
+		Serial::Read(handler->Interface, buff, MSG_TOTAL_LEN*2);
+		
+	}
 }
--- SystemSimulation/Messaging.h
+++ SystemSimulation/Messaging.h
@@ -11,23 +11,27 @@
 
 //int ParseMessages(void *buffer, u8 *messages);
 
-#define MESSAGE_BUFF_LEN 10
+#define MSG_BUFF_LEN 10
 
-#define LABEL_LEN 16
-#define CONTENT_LEN 128
-//#define Message_Len
+#define MSG_DEST_LEN 16
+#define MSG_LABEL_LEN 16
+#define MSG_CONTENT_LEN 128
+#define MSG_TOTAL_LEN (MSG_DEST_LEN + MSG_LABEL_LEN + MSG_CONTENT_LEN)
 
 struct Message
 {
-	char Label[LABEL_LEN];
-	char Content[CONTENT_LEN];
+	char Dest[MSG_DEST_LEN];
+	char Label[MSG_LABEL_LEN];
+	char Content[MSG_CONTENT_LEN];
 };
 
 struct MessageHandler 
 {
 	Serial *Interface;
-	Message Message[MESSAGE_BUFF_LEN];
-	u64 Head, Tail;
+	Message Messages[MSG_BUFF_LEN];
+	char Buffer[MSG_TOTAL_LEN*2];
+	u64 MsgHead, MsgTail, BuffHead, BuffTail;
+	
 };
 
 void InitMessager(MessageHandler *handler, Serial *interface);
