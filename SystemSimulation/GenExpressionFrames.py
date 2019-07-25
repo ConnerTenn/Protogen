f3f7703a781007b508bca1370af39b0b921e200c
Conner <connertenn@gmail.com>
2019-07-25 07:20:23 -0400

    Implementing frames parser

--- SystemSimulation/GenExpressionFrames.py
+++ SystemSimulation/GenExpressionFrames.py
@@ -1,20 +1,35 @@
 #!/bin/python
 
 
-import sys
 
+f=None
+fframes="Frames.dat"
+fexpr="Expressions.dat"
+fExpressionDataH=None #"ExpressionData.h"
+fExpressionDataCpp=None #"ExpressionData.cpp"
+fFrameDataH=None #"FrameData.h"
+fFrameDataCpp=None #"FrameData.cpp"
+try: f = open(fframes, 'r')
+except: print("Error opening file \"{0}\"".format(fframes)); exit(-1)
+fframes=f
+try: f = open(fexpr, 'r')
+except: print("Error opening file \"{0}\"".format(fexpr)); exit(-1)
+fexpr=f
 
+try: f = open(fExpressionDataH, 'w')
+except: print("Error opening file \"{0}\"".format(fExpressionDataH)); exit(-1)
+fExpressionDataH=f
+try: f = open(fExpressionDataCpp, 'w')
+except: print("Error opening file \"{0}\"".format(fExpressionDataCpp)); exit(-1)
+fFrameDataH=f
+try: f = open(fFrameDataH, 'w')
+except: print("Error opening file \"{0}\"".format(fFrameDataH)); exit(-1)
+fFrameDataCpp=f
+try: f = open(fFrameDataCpp, 'w')
+except: print("Error opening file \"{0}\"".format(fFrameDataCpp)); exit(-1)
+fFrameDataCpp=f
 
-if len(sys.argv) > 2:
-	print("Usage: ./GenImageData.py\n")
-	exit(-1)
-
-
-
-file=None
-try: file = open(sys.argv[1], 'r')
-except: print("Error opening file"); exit(-1)
-
+exit(0)
 
 buff=""
 for line in file:
