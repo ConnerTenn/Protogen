09603496e53828e508a6b46a9b7ae5d0a64030dc
Conner <connertenn@gmail.com>
2019-10-27 16:23:16 -0400

    Added FrameData code

---
+++ AvrCode/frames.c
@@ -0,0 +1,5 @@
+
+#include "frames.h"
+
+const u8 *FrameData = (const u8 *)(&_binary_FrameData_bin_start);
+const u8 *FrameDataEnd = (const u8 *)(&_binary_FrameData_bin_end);
---
+++ AvrCode/frames.h
@@ -0,0 +1,8 @@
+
+#include "globals.h"
+
+extern const char _binary_FrameData_bin_start;
+extern const char _binary_FrameData_bin_end;
+extern const u8 *FrameData;
+extern const u8 *FrameDataEnd;
+
