02d13a432ad950494f5a222337a6f4689275ba8d
Conner Tenn <connertenn@gmail.com>
2019-10-29 17:01:20 -0400

    Implemented left Frame translation

--- AvrCode/frames.c
+++ AvrCode/frames.c
@@ -48,19 +48,36 @@ void FrameTransLeft(u8 *data, u8 numDisplays, u8 num)
 }
 
 
+/*
+
 def FrameTransLeft(data, numDisplays, num):
 	segover = CEILDIV(num,8)
 	segov = int(int(num)/int(8)+1)
-	bitover=num%8
+	bitover = num%8
 	for y in range(0, 8): #(int y=0; y<8-num; y--)
 		for x in range(0,numDisplays): #(int x=0; x<width; x++)
 			if x+segov-1<numDisplays:
 				data[y*numDisplays+x] = ( (data[y*numDisplays+x+segov-1]<<bitover) & (0xFF<<bitover) )
 			else:
-				data[y*numDisplays+x]=0
+				data[y*numDisplays+x] = 0
 			if x+segover<numDisplays:
 				data[y*numDisplays+x] |= ( ((data[y*numDisplays+x+segover]) & (~(0xFF>>bitover))) >> (8-bitover) )
 			data[y*numDisplays+x] = data[y*numDisplays+x]&0xFF
-					
+
+def FrameTransRight(data, numDisplays, num):
+	segover = CEILDIV(num,8)
+	segov = int(int(num)/int(8)+1)
+	bitover = num%8
+	for y in range(0, 8): #(int y=0; y<8-num; y--)
+		for x in range(numDisplays-1,-1,-1): #(int x=0; x<width; x++)
+			if x-segov+1>=0:
+				data[y*numDisplays+x] = ( (data[y*numDisplays+x-segov+1]>>bitover) & (0xFF>>bitover) )
+			else:
+				data[y*numDisplays+x] = 0
+			if x-segover>=0:
+				data[y*numDisplays+x] |= ( ((data[y*numDisplays+x-segover]) & (~(0xFF<<bitover))) << (8-bitover) )
+			data[y*numDisplays+x] = data[y*numDisplays+x]&0xFF
+
+*/
 
 
