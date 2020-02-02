1113f81354d87107431be125031efa72c82cb1a3
Conner <connertenn@gmail.com>
2020-02-02 18:56:30 -0500

    Added phase offset for interrupt timers

--- AvrCode/main.c
+++ AvrCode/main.c
@@ -20,7 +20,7 @@ Max7219 DisplayList[] =
 		{1, 1, -1},
 		{4, 3, -1},
 	};
-u8 NumDisplays = 3;
+u8 NumDisplays;
 u8 TotalSegments;
 #define CSPIN 0
 
@@ -29,6 +29,8 @@ u8 TotalSegments;
 ISR(TIMER1_COMPA_vect)
 {
 	sei(); //Enable Nested Interrupts
+
+	ENBITS(PORTC, 0x2);
 
 	while (SerialAvail()>=4)
 	{
@@ -82,6 +84,8 @@ ISR(TIMER1_COMPA_vect)
 		numSegments[d] = DisplayList[d].NumSegments;
 	}
 	Max7219SendData(data, numSegments, NumDisplays, CSPIN);
+
+	DABITS(PORTC, 0x2);
 }
 
 //Frame delay update
@@ -89,7 +93,9 @@ ISR(TIMER0_COMPA_vect)
 {
 	sei(); //Enable Nested Interrupts
 
-	for (u8 i=0; i<sizeof(DisplayList)/sizeof(DisplayList[0]); i++)
+	ENBITS(PORTC, 0x4);
+
+	for (u8 i=0; i<NumDisplays; i++)
 	{
 		if (DisplayList[i].FrameDelay!=-1)
 		{
@@ -102,27 +108,32 @@ ISR(TIMER0_COMPA_vect)
 		}
 	}
 	
+	DABITS(PORTC, 0x4);
 }
 
 void InitTimers()
 {
+	//Counter 0 (8 bit)
+	//Page 84
+	TCCR0A = (1<<WGM01);
+	OCR0A = 250 - 1; //(16MHz/64)/1000Hz  1KHz
+	TCNT0 = 0;
+	TIMSK0 = (1<<OCIE0A); //Enable Interrupt
+	//CTC mode & Prescaler divide by 64 & start the timer
+	TCCR0B = (0<<CS02)|(1<<CS01)|(1<<CS00); 
+
+	//Offset Counter 1 by 1/16 of 1ms (of the overflow value of Counter 0)
+	while (TCNT0<OCR0A/16) {} //Wait till offset is reached
+
 	//Counter 1 (16 bit)
 	//Page 89
 	//Page 108
-	OCR1A = 2000; //(16MHz/8)/1000Hz  1KHz
+	OCR1A = 2000 - 1; //(16MHz/8)/1000Hz  1KHz
 	TCNT1 = 0;
 	TIMSK1 = (1<<OCIE1A); //Enable Interrupt
 	//CTC mode & Prescale divide by 8 & start the timer
 	TCCR1B = (1<<WGM12) | (0<<CS12)|(1<<CS11)|(0<<CS10); 
 	
-	//Counter 0 (8 bit)
-	//Page 84
-	TCCR0A = (1<<WGM01);
-	OCR0A = 250; //(16MHz/64)/1000Hz  1KHz
-	TCNT0 = 0;
-	TIMSK0 = (1<<OCIE0A); //Enable Interrupt
-	//CTC mode & Prescaler divide by 64 & start the timer
-	TCCR0B = (0<<CS02)|(1<<CS01)|(1<<CS00); 
 }
 
 int main()
@@ -132,6 +143,8 @@ int main()
 	DDRC = 0xF;
 
 	IntiSPI();
+
+	NumDisplays = sizeof(DisplayList)/sizeof(DisplayList[0]);
 
 	TotalSegments=0;
 	for (u8 i=0; i<NumDisplays; i++)
