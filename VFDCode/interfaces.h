bb746a68ecbe5d2a15b4e85bb2c47461942c722f
Conner <connertenn@gmail.com>
2020-02-11 23:21:47 -0500

    Added Code Framework for VFD

---
+++ VFDCode/interfaces.h
@@ -0,0 +1,44 @@
+
+#include "globals.h"
+#include <avr/interrupt.h>
+
+//http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+
+#define SETBITS(r,m,v) ((r)=((r) & ~(m)) | ((m) & (v)))
+#define ENBITS(r,m) ((r)=((r) | (m)))
+#define DABITS(r,m) ((r)=((r) & ~(m)))
+
+//Page 135
+void IntiSPI();
+void SPITransmit(const u8 data);
+void SPITransmit16(const u16 data);
+
+#define DDRB_MOSI 3
+#define DDRB_SCK 5
+#define DDRB_CS 2
+
+#define REFRESH_INTERVAL 100
+u16 RefreshTimer;
+
+//Page 143
+void IntiUART();
+void SerialTransmitByte(u8 data);
+//void SerialTransmit(u8 *data, u8 len);
+void SerialFlush();
+void SerialTransmit(u8 *data, u8 len);
+void SerialTransmitStr(char *data);
+
+u8 SerialRead(u8 *data, u8 len);
+u8 SerialAvail();
+
+
+
+
+void Max7219Init(u8 numSegments, u8 cs);
+void Max7219Refresh(u8 numSegments, u8 cs);
+void Max7219SendCmd(u16 cmd, u8 numSegments, u8 cs);
+// void Max7219Send4Frame(u8 *data, u8 cs);
+// void Max7219Send2Frame(u8 *data, u8 cs);
+// void Max7219Send1Frame(u8 *data, u8 cs);
+
+void Max7219SendData(u8 **data, u8 *numSegments, u8 numDisplays, u8 cs);
---
+++ VFDCode/main.c
@@ -0,0 +1,96 @@
+
+#include "globals.h"
+
+#include <stdio.h>
+
+#include "interfaces.h"
+
+
+
+//main callback
+ISR(TIMER1_COMPA_vect)
+{
+	sei(); //Enable Nested Interrupts
+
+	ENBITS(PORTC, 0x2);
+
+	while (SerialAvail()>=4)
+	{
+		/*
+		New serial format:
+		u8 DeviceAddr
+		u8 DestDisplay (implies Display type)
+		u16 FrameNumber
+		u16 MaskNumber
+		u16 BorderNumber
+		*/
+		u8 in[4];
+		SerialRead(in, 4);
+
+		
+	}
+
+
+
+	DABITS(PORTC, 0x2);
+}
+
+//Frame delay update
+ISR(TIMER0_COMPA_vect)
+{
+	sei(); //Enable Nested Interrupts
+
+	ENBITS(PORTC, 0x4);
+
+
+	DABITS(PORTC, 0x4);
+}
+
+void InitTimers()
+{
+	//Counter 0 (8 bit)
+	//Page 84
+	TCCR0A = (1<<WGM01);
+	OCR0A = 250 - 1; //(16MHz/64)/1000Hz  1KHz
+	TCNT0 = 0;
+	TIMSK0 = (1<<OCIE0A); //Enable Interrupt
+	//CTC mode & Prescaler divide by 64 & start the timer
+	TCCR0B = (0<<CS02)|(1<<CS01)|(1<<CS00); 
+
+	//Offset Counter 1 by 1/16 of 1ms (of the overflow value of Counter 0)
+	while (TCNT0<OCR0A/16) {} //Wait till offset is reached
+
+	//Counter 1 (16 bit)
+	//Page 89
+	//Page 108
+	OCR1A = 2000 - 1; //(16MHz/8)/1000Hz  1KHz
+	TCNT1 = 0;
+	TIMSK1 = (1<<OCIE1A); //Enable Interrupt
+	//CTC mode & Prescale divide by 8 & start the timer
+	TCCR1B = (1<<WGM12) | (0<<CS12)|(1<<CS11)|(0<<CS10); 
+	
+}
+
+int main()
+{
+	PRR = 0;//PRR & ~(1<<PRSPI); //Power Reduction Register
+
+	DDRC = 0xF;
+
+	IntiSPI();
+
+	
+	//Max7219Init(TotalSegments, CSPIN);
+	IntiUART();
+
+	InitTimers();
+
+	SerialTransmit("\nSetup Complete\n", 17);
+
+	while (1)
+	{
+
+	}
+
+	return 0;
+}
