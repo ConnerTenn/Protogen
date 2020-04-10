acaeea3cb7959304ca3568c9e9626982f4d5bb83
Conner <connertenn@gmail.com>
2020-04-10 18:16:28 -0400

    Some Rearranging and Optimization

--- Chest_AvrCode/buttonCtl.c
+++ Chest_AvrCode/buttonCtl.c
@@ -26,87 +26,17 @@ void InitButtons()
 	// SerialTransmitStr("\n");
 
 	// SerialTransmitStr("Buttons:\n");
-	for (u8 b = 0; b < NUM_BUTTONS; b++)
-	{
-		Buttons[b].Pressed = 0;
-		Buttons[b].Active = 0;
-		Buttons[b].ButtonID = BUTTONDATA_ACC_8(roff); roff+=1;
-	
-		// PRINT_VAL("\tButtonID:", Buttons[b].ButtonID);
-	}
-
-	// SerialTransmitStr("Combos:\n");
-	for (u8 c = 0; c < NUM_COMBOS; c++)
-	{
-		Combos[c] = (Combo *)woff;
-
-		Combos[c]->Active = 0; woff+=1;
-		Combos[c]->NumButtons = BUTTONDATA_ACC_8(roff); roff+=1; woff+=1;
-		// PRINT_VAL("\tNumButtons:", Combos[c]->NumButtons);
-
-		for (u8 b = 0; b < Combos[c]->NumButtons; b++)
-		{
-			u8 buttonidx = BUTTONDATA_ACC_8(roff); roff+=1;
-			Combos[c]->Buttons[b] = &(Buttons[buttonidx]); woff+=2;
-			// PRINT_VAL("\t\tButtonIdx:", buttonidx);
-		}
-
-	}
-
-
-	// SerialTransmitStr("Sequences:\n");
-	for (u8 s = 0; s < NUM_SEQUENCES; s++)
-	{
-		Sequences[s] = (Sequence *)woff;
-
-		Sequences[s]->ActiveCombo = 0; woff+=1;
-		Sequences[s]->Momentary = BUTTONDATA_ACC_8(roff); roff+=1; woff+=1;
-		// SerialTransmitStr("\tMomentary:"); SerialTransmitStr(Sequences[s]->Momentary ? "Y\n" : "N\n");
-
-		Sequences[s]->CommandLen = BUTTONDATA_ACC_8(roff); roff+=1; woff+=1;
-		u16 cmdoff = BUTTONDATA_ACC_16(roff); roff+=2;
-		Sequences[s]->Command = CommandData + cmdoff; woff+=2;
-		// PRINT_VAL("\tCommandLen:", Sequences[s]->CommandLen);
-		// PRINT_VAL("\tCommandOff:", cmdoff);
-
-		Sequences[s]->NumCombos = BUTTONDATA_ACC_8(roff); roff+=1; woff+=1;
-		// PRINT_VAL("\tNumCombos:", Sequences[s]->NumCombos);
-		for (u8 c = 0; c < Sequences[s]->NumCombos; c++)
-		{
-			u8 comboidx = BUTTONDATA_ACC_8(roff); roff+=1;
-			Sequences[s]->Combos[c] = Combos[comboidx]; woff+=2;
-			// PRINT_VAL("\t\tComboIdx:", comboidx);
-		}
-	}
-
 	u8 pB=0, pC=0, pD=0;
 	for (u8 b = 0; b < NUM_BUTTONS; b++)
 	{
-		/*
-		 0: B, bit0 //D8
-		 1: B, bit1 //D9
-		 2: B, bit2 //D10
-		 3: B, bit3 //D11
-		 4: B, bit4 //D12
-		 5: B, bit5 //D13
+		// Buttons[b].Pressed = 0;
+		// Buttons[b].Active = 0;
+		Buttons[b].ButtonID = BUTTONDATA_ACC_8(roff); roff+=1;
+	
+		// PRINT_VAL("\tButtonID:", Buttons[b].ButtonID);
 
-		 6: C, bit0 //A0
-		 7: C, bit1 //A1
-		 8: C, bit2 //A2
-		 9: C, bit3 //A3
-		10: C, bit4 //A4
-		11: C, bit5 //A5
-
-		12: D, bit0 //D0
-		13: D, bit1 //D1
-		14: D, bit2 //D2
-		15: D, bit3 //D3
-		16: D, bit4 //D4
-		17: D, bit5 //D5
-		18: D, bit6 //D6
-		19: D, bit7 //D7
-		*/
-
+		
+		//Setup DDR
 #define CFG_BUTTON_PIN(port, bit) p##port |= (1<<bit);
 		u8 id = Buttons[b].ButtonID;
 		if (id<=5)
@@ -126,6 +56,60 @@ void InitButtons()
 	PORTB|=pB; PORTC|=pC; PORTD|=pD;
 	pB=~pB;    pC=~pC;    pD=~pD;
 	DDRB&=pB;  DDRC&=pC;  DDRD&=pD;
+
+	// SerialTransmitStr("Combos:\n");
+	for (u8 c = 0; c < NUM_COMBOS; c++)
+	{
+		Combos[c] = (Combo *)woff;
+
+		// Combos[c]->Active = 0;
+		Combos[c]->NumButtons = BUTTONDATA_ACC_8(roff); roff+=1;
+		// PRINT_VAL("\tNumButtons:", Combos[c]->NumButtons);
+
+		woff+=1+1;
+
+		for (u8 b = 0; b < Combos[c]->NumButtons; b++)
+		{
+			u8 buttonidx = BUTTONDATA_ACC_8(roff); roff+=1;
+			Combos[c]->Buttons[b] = &(Buttons[buttonidx]);
+			// PRINT_VAL("\t\tButtonIdx:", buttonidx);
+
+			woff+=2;
+		}
+
+	}
+
+
+	// SerialTransmitStr("Sequences:\n");
+	for (u8 s = 0; s < NUM_SEQUENCES; s++)
+	{
+		Sequences[s] = (Sequence *)woff;
+
+		Sequences[s]->ActiveCombo = 0;
+		Sequences[s]->Momentary = BUTTONDATA_ACC_8(roff); roff+=1;
+		// SerialTransmitStr("\tMomentary:"); SerialTransmitStr(Sequences[s]->Momentary ? "Y\n" : "N\n");
+
+		Sequences[s]->CommandLen = BUTTONDATA_ACC_8(roff); roff+=1;
+		u16 cmdoff = BUTTONDATA_ACC_16(roff); roff+=2;
+		Sequences[s]->Command = CommandData + cmdoff;
+		// PRINT_VAL("\tCommandLen:", Sequences[s]->CommandLen);
+		// PRINT_VAL("\tCommandOff:", cmdoff);
+
+		Sequences[s]->NumCombos = BUTTONDATA_ACC_8(roff); roff+=1;
+
+		woff+=1+1+1+2+1;
+
+		// PRINT_VAL("\tNumCombos:", Sequences[s]->NumCombos);
+		for (u8 c = 0; c < Sequences[s]->NumCombos; c++)
+		{
+			u8 comboidx = BUTTONDATA_ACC_8(roff); roff+=1;
+			Sequences[s]->Combos[c] = Combos[comboidx];
+			// PRINT_VAL("\t\tComboIdx:", comboidx);
+
+			woff+=2;
+		}
+	}
+
 
 }
 
@@ -221,46 +205,20 @@ void UpdateButtons()
 		for (u8 s = 0; s < NUM_SEQUENCES; s++)
 		{
 			Sequences[s]->ActiveCombo = 0;
-			for (u8 c = 0; c < Sequences[s]->NumCombos; c++)
-			{
-				Sequences[s]->Combos[c]->Active = 0;
-				// for (u8 b = 0; b < Sequences[s]->Combos[c]->NumButtons; b++)
-				// {
-				// 	Sequences[s]->Combos[c]->Buttons[b].Active = 0;
-				// }
-			}
+			// for (u8 c = 0; c < Sequences[s]->NumCombos; c++)
+			// {
+			// 	Sequences[s]->Combos[c]->Active = 0;
+			// 	// for (u8 b = 0; b < Sequences[s]->Combos[c]->NumButtons; b++)
+			// 	// {
+			// 	// 	Sequences[s]->Combos[c]->Buttons[b].Active = 0;
+			// 	// }
+			// }
 		}
 	}
 }
 
 u8 ReadButton(u8 id)
 {
-
-	/*
-	0: B, bit0 //D8
-	1: B, bit1 //D9
-	2: B, bit2 //D10
-	3: B, bit3 //D11
-	4: B, bit4 //D12
-	5: B, bit5 //D13
-
-	6: C, bit0 //A0
-	7: C, bit1 //A1
-	8: C, bit2 //A2
-	9: C, bit3 //A3
-	10: C, bit4 //A4
-	11: C, bit5 //A5
-
-	12: D, bit0 //D0
-	13: D, bit1 //D1
-	14: D, bit2 //D2
-	15: D, bit3 //D3
-	16: D, bit4 //D4
-	17: D, bit5 //D5
-	18: D, bit6 //D6
-	19: D, bit7 //D7
-	*/
-
 #define GET_INPUT(port, bit) (PIN##port & (1<<(bit)))
 
 	u8 v = 0;
--- Chest_AvrCode/buttonCtl.h
+++ Chest_AvrCode/buttonCtl.h
@@ -11,7 +11,35 @@ extern PROGMEM const char *const _binary_ButtonData_bin_end;
 
 
 #define PRINT_VAL(txt, val) SerialTransmitStr(txt); SerialTransmitHexVal(val); SerialTransmitStr("\n"); SerialFlush();
-	
+
+
+/*
+Pin Mapping:
+
+0: B, bit0 //D8
+1: B, bit1 //D9
+2: B, bit2 //D10
+3: B, bit3 //D11
+4: B, bit4 //D12
+5: B, bit5 //D13
+
+6: C, bit0 //A0
+7: C, bit1 //A1
+8: C, bit2 //A2
+9: C, bit3 //A3
+10: C, bit4 //A4
+11: C, bit5 //A5
+
+12: D, bit0 //D0
+13: D, bit1 //D1
+14: D, bit2 //D2
+15: D, bit3 //D3
+16: D, bit4 //D4
+17: D, bit5 //D5
+18: D, bit6 //D6
+19: D, bit7 //D7
+*/
+
 /*
 Binart Data Segment:
 
